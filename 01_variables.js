7f4ef4fd018c2fa68eaf90f929ff20f9288f5f2d
RishabhCubastion <103982858+RishabhCubastion@users.noreply.github.com>
2025-05-14 18:34:55 +0000

    datatypes

---
+++ 01_variables.js
@@ -0,0 +1,9 @@
+const accountId = 144553
+let accountEmail = 'rishabh@google.com'
+var accountPassword = "12345"
+accountCity = "Jaiput"
+let pankaj;
+
+console.table([accountEmail,accountPassword,accountCity,pankaj])
+
+/*Prefer not to use var because of issue in block scope and functional scope*/
---
+++ 02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict" // treat all JS Code as newwer version --treat it as new standards
+
+alert("hello") //we are using node js not browser, so it will run here
+
+console.log(3+
+    3) // code readability should be high
+
+
+//documentation = tc39(official documentation) and mdm (best dosumentation)
+
+
+
+//number => 2 to power 53 that is the range
+//bigint => in case if number is quite bigger than range
+// string ->prefer in double quotes
+// boolean => true/false
+// null => standalone value
+// undefined => value is not assigned
+//symbol => unique
+
+//object
+
+console.log(typeof "hitesg") // string
+//but interstingly we have
+console.log(typeof null) //object ------Important
+console.log(typeof undefined) //undefined --------- Important
