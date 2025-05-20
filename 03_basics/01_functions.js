0d77ccebc9d477d42f0243517aec03a0079fa385
RishabhCubastion <103982858+RishabhCubastion@users.noreply.github.com>
2025-05-20 02:02:16 +0000

    functions basics done

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,45 @@
+function sayMyName(){} //definition of function
+sayMyName() //execution of function
+sayMyName //this is just a reference it will not execute. to execute we have to add()
+
+
+function addTwoNumbers (num1,num2){
+    console.log(num1+num2)
+}
+addTwoNumbers() //we get NaN as an output because we have not passed arguement
+addTwoNumbers(2,3) //5
+addTwoNumbers("2",3) //23
+addTwoNumbers(2,"q") //2q
+
+//parameters vs arguments
+//when we define  the function then the num1, num2 is called as parameters
+// and when we call the function and then 2,3 is known as arguments
+
+// we can also take the functions inside a variable
+const result = addTwoNumbers(3,5); //8
+console.log(result); //undefined, this is undefined because in that function we are creating the console only, we have not returned the result of function
+
+//to return the result we have somewhat like:
+function addTwoNumber(num1,num2){
+    let result = num1+num2;
+    return result //now it acts in a good way
+}
+
+//Note: After return statment we can't execute anything
+
+
+function loginUserMessage(username){
+    return `${username} just logged in`
+}
+console.log(loginUserMessage("Rishabh")); //Rishabh just logged in
+console.log(loginUserMessage()); //Note : undefined just logged in 
+
+
+// we also have process to check
+function loginUserMessage(username){
+    if(username===undefined){ //is equivalent to if(!username)
+        console.log("Please enter the username");
+        return //so thet the below code will not execute
+    }
+    return `${username} just logged in`
+}
