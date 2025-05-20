247c1364b355d19957eb1f6ffa4f4ce0c3160679
RishabhCubastion <103982858+RishabhCubastion@users.noreply.github.com>
2025-05-20 18:06:09 +0000

    scopes done

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -27,3 +27,38 @@ console.log(c); //30 and this is a problem
 
 
 //Note: The global scope inside the node and browser environment is different from each other
+
+
+
+//Nested Scope
+function one(){
+    const username = "Rishabh"
+
+    function two(){
+        const website = "youtube"
+        console.log(username); // can we access the username inside this function  --yes and it prints "Rishabh"
+    }
+    console.log(website);// can we access the website inside this function - no
+    //website is not defined
+    
+    two()
+}
+
+one()
+
+//So in conclusion : the child function can access the parent but the vice versa is not true
+//same process will repeat in nested if else as well
+
+
+
+//+++++++++++++++++++++++++++++++++++++++++++++++interesting+++++++++++++++++++++++++++++++++
+
+function addone(num){
+    return num +1;
+}
+addone(5); // this can also be written before function addone declaration
+//the same thing is also exists with different ways like function which hold inside a variable
+const addTwo = function(num){
+    return num+2
+}
+addTwo(5) //now if we place this before declaration of addTwo then the error : cannot access 'addTwo' before initialization
