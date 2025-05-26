496eb3c4c3a698f494693c0b926fe10a2263167c
RishabhCubastion <103982858+RishabhCubastion@users.noreply.github.com>
2025-05-26 01:43:38 +0000

    while_dowhile done

---
+++ 05_iterations/while_do_while.js
@@ -0,0 +1,26 @@
+let index=0;
+
+while (index<=10) {
+    console.log(`value of index is ${index}`);
+    index = index+2
+}
+
+
+let myArray = ["flash","batman","superman"]
+let arr = 0;
+while (arr<myArray.length) {
+    console.log(`Value is ${myArray[arr]}`);
+    arr=arr+1;
+    
+}
+
+
+
+//DO WHILE LOOP
+//ye bolta hn ki pehle kaam hoga then condition check hogi
+let score = 1 //and agar score ki value 11 hogi to
+do {
+    console.log(`Score is ${score}`);
+    score++
+    
+} while (score<=10); // to 1 baar loop chalega then fir condition check hogi
